22082db3003a06b7254f91014a85c4c7fb0a89b9
Simon Marchi <simon.marchi@polymtl.ca>
2016-04-12 08:08:04 -0400

    wip

--- test.c
+++ test.c
@@ -1,5 +1,6 @@
 #include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 #define FLASH_SIZE (64 * 1024 * 1024)
 
@@ -13,22 +14,34 @@ void print_uart0(const char *s) {
 }
 
 extern char flash_start;
+extern char heap_start;
+extern char heap_end;
 
 int main() {
-    char buf[20];
+    char *buf;
     int i;
     int *p = (int *) &flash_start;
-    const int num_ints = FLASH_SIZE / sizeof(int);
+    const int num_ints = 20;
 
     print_uart0("Salut!\n");
-    
+
+    sprintf(buf, "Heap start: %p\n", &heap_start);
+    print_uart0(buf);
+
+    sprintf(buf, "Heap end: %p\n", &heap_end);
+    print_uart0(buf);
+
     sprintf(buf, "Flash address: %p\n", flash_start);
     print_uart0(buf);
-    
+
     for (i = 0; i < num_ints; i++) {
 	sprintf(buf, "Value %08x\n", p[i]);
 	print_uart0(buf);
     }
+
+    buf = malloc(8888);
+    sprintf(buf, "Buf address: %p\n", buf);
+    print_uart0(buf);
 
     return 0;
 }
